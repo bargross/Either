e7f05c548f7f38a5039b0369555ac6654a0e5b24
Bargross <barggros@gmail.com>
2020-01-05 02:37:52 +0000

    removed expressions and added method to replace validation functions

--- Either/EitherTest/Either/EitherTests.cs
+++ Either/EitherTest/Either/EitherTests.cs
@@ -5,7 +5,7 @@ using Either.Rule;
 
 namespace EitherTest
 {
-    public class Tests
+    public class EitherTests
     {
         private Either<string, int> _either;
         private RuleValidator<string, int> _validator;
@@ -14,6 +14,14 @@ namespace EitherTest
         public void Setup()
         {
             _validator = new RuleValidator<string, int>();
+        }
+
+        [Test]
+        public void EitherConstructor_NoParametersProvided_ValidatorInstantiated()
+        {
+            _either = new Either<string, int>();
+
+            Assert.IsTrue(_either.ValidatorInstantiated);
         }
 
         [Test]
